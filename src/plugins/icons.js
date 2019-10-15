38319919fc518a4381d100b0d6716cf1ee21f529
Adrian Gaborek <unity3d09@gmail.com>
2019-10-15 11:59:39 +0200

    refactor: create 2 new files for axios and icons configuration in main.js.

---
+++ src/plugins/icons.js
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLock, faHeart, faUserPlus, faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faLock)
+library.add(faHeart)
+library.add(faUserPlus)
+library.add(faUser)
