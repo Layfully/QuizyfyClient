cf9469c18622afc7f17f3e26cac7551c1d6ffaaa
Adrian Gaborek <unity3d09@gmail.com>
2019-10-15 11:30:58 +0200

    refactor: move axios configuration to diffrent file to declutter main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
-import Axios from 'axios'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import { onSuccess, onError, beforeRequestSuccess, beforeRequestError } from '@/interceptors/jwt'
 import VuePageTransition from 'vue-page-transition'
 import vuetify from './plugins/vuetify'
+import Axios from './plugins/axios'
 import './plugins/icons'
 import 'vue-material-design-icons/styles.css'
 import { VueReCaptcha } from 'vue-recaptcha-v3'
@@ -17,18 +16,7 @@ Vue.use(VuePageTransition)
 
 Vue.config.productionTip = process.env.NODE_ENV === 'development'
 
-Axios.defaults.baseURL = process.env.API_URL
-Axios.interceptors.request.use(beforeRequestSuccess, beforeRequestError)
-Axios.interceptors.response.use(onSuccess, onError)
-
 Vue.prototype.$http = Axios
-
-const jwtToken = localStorage.getItem('access_token')
-
-if (jwtToken) {
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = jwtToken
-}
-
 Vue.prototype.$eventBus = new Vue()
 
 /* eslint-disable no-new */
