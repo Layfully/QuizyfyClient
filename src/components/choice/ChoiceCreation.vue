1ccf58be7ed460e973e10ec3debe2029222af99e
Adrian Gaborek <unity3d09@gmail.com>
2019-10-22 11:12:20 +0200

    refactor: introduce new components for quiz creator, which use vuetify components and look better.

---
+++ src/components/choice/ChoiceCreation.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <InputField
+    :name="'Treść odpowiedzi ' + (index + 1)"
+    type="text"
+    outlined
+    dense
+    :validationRules="{ require:true }"
+    v-model="choice.text"/>
+    <v-switch v-model="choice.isRight" :label="switchLabel" @change="emitChoiceChangeEvent"></v-switch>
+  </div>
+</template>
+
+<script>
+import InputField from '@/components/InputField'
+
+export default {
+  name: 'ChoiceCreation',
+  components: {
+    InputField
+  },
+  props: { index: { required: true } },
+  data () {
+    return {
+      choice: {
+        text: '',
+        isRight: false
+      }
+    }
+  },
+  methods: {
+    emitChoiceChangeEvent () {
+      this.$emit('choiceChange', this.choice, this.index)
+    }
+  },
+  computed: {
+    switchLabel () {
+      return this.choice.isRight ? 'Odpowiedź poprawna' : 'Odpowiedź niepoprawna'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/quiz/QuizCreation.vue
+++ src/views/quiz/QuizCreation.vue
@@ -1,108 +1,73 @@
 <template>
-  <b-container fluid>
-    <div class="form-wrapper">
-      <b-form @submit.prevent="createQuiz" enctype="multipart/form-data">
-        <b-form-group
-          :label-cols="2"
-          label-size="lg"
-          breakpoint="md"
-          horizontal
-          label="Nazwa quizu"
-          for="name">
-          <b-col :md="5" class="center">
-            <b-input
-              id="name"
-              v-model="quiz.name"
-              maxlength="60"
-              required />
-          </b-col>
-        </b-form-group>
-
-        <b-form-group
-          :label-cols="2"
-          label-size="lg"
-          breakpoint="md"
-          horizontal
-          label="Opis"
-          for="description">
-          <b-col :md="5" class="center">
-            <b-form-textarea
-              v-model="quiz.description"
-              id="description"
-              placeholder="Opis..."
-              rows="3"
-              max-rows="8"
-              required
-            ></b-form-textarea>
-          </b-col>
-        </b-form-group>
-
-        <b-form-group
-          :label-cols="2"
-          label-size="lg"
-          breakpoint="md"
-          horizontal
-          label="Thumbnail:"
-          for="Image">
-          <b-col :md="5" class="center">
-              <b-form-file
-                id="image"
-                accept=".jpg, .png, .gif"
-                v-model="image"
-                :state="Boolean(file)"
-                placeholder="Choose a file..."
-                drop-placeholder="Drop file here..."
-                required
-              ></b-form-file>
-          </b-col>
-        </b-form-group>
-
-        <b-col
-          :md="5"
-          class="center flex-center"
-          v-if="!this.shouldStartQuestionCreation"
-          offset="4">
-          <b-button
-            v-on:click.once="startQuestionCreation"
-            variant="info">Dodaj pytania</b-button>
-          <b-button
-            :to="{ name: 'OwnerList' }"
-            variant="danger">Anuluj</b-button>
-        </b-col>
-        <div v-if="this.shouldStartQuestionCreation">
-          <template v-for="(question, index) in quiz.questions">
-            <QuestionCreate :index=index :key="index" @questionChange='applyChangedQuestion'/>
-          </template>
-          <b-col
-            :md="5"
-            class="center flex-center"
-            offset="4">
-            <b-button @click="addNewQuestion" variant="info">Dodaj kolejne pytanie</b-button>
-            <b-button @click="createQuiz" variant="success">Utwórz quiz</b-button>
-          </b-col>
-        </div>
-      </b-form>
-    </div>
-  </b-container>
+  <v-layout justify-center>
+    <v-flex xs11 sm9 md6>
+      <v-form>
+        <v-card>
+          <v-card-title class="text-center d-block">Kreator quizów</v-card-title>
+          <v-card-text>
+            <InputImage v-model="quiz.image" class="my-12">
+              <div slot="activator">
+                <v-img v-ripple v-if="!quiz.image.imageURL" class="grey lighten-3">
+                  <v-layout justify-center align-center style="height:150px; cursor:pointer">
+                    <Camera class="icon"></Camera>
+                    <span>Dodaj zdjęcie</span>
+                  </v-layout>
+                </v-img>
+                <v-img v-ripple v-else :src="quiz.image.imageURL">
+                </v-img>
+              </div>
+            </InputImage>
+            <InputField
+              name="Nazwa quizu"
+              type="text"
+              outlined
+              :validationRules="{ require:true }"
+              dense
+              v-model="quiz.name"/>
+            <v-textarea
+              auto-grow
+              dense
+              clearable
+              outlined
+              label="Opis quizu"
+              rows="1"
+              v-model="quiz.description"/>
+            <QuestionCreation v-for="(question, index) in quiz.questions" :index=index :key="index" @questionChange="questionChange()"/>
+          </v-card-text>
+          <v-card-actions>
+            <v-layout justify-center>
+              <v-btn large color="primary" @click="addQuestion()">Dodaj pytanie</v-btn>
+              <v-btn large color="success" @click="createQuiz()">Utwórz quiz</v-btn>
+            </v-layout>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import QuizService from '@/api-services/quiz.service'
-import QuestionCreate from '@/components/question/QuestionCreate.vue'
+import InputField from '@/components/InputField'
+import InputImage from '@/components/InputImage'
+import QuestionCreation from '@/components/question/QuestionCreation'
+import Camera from 'vue-material-design-icons/Camera'
 
 export default {
   name: 'QuizCreation',
   components: {
-    'QuestionCreate': QuestionCreate
+    InputField,
+    InputImage,
+    QuestionCreation,
+    Camera
   },
   data () {
     return {
       quiz: {
         name: '',
         description: '',
-        questions: []
-      },
-      image: '',
-      shouldStartQuestionCreation: false
+        questions: [],
+        image: {}
+      }
     }
   },
   methods: {
@@ -119,43 +84,15 @@ export default {
         alert(error)
       })
     },
-    startQuestionCreation () {
-      this.shouldStartQuestionCreation = true
-      this.addNewQuestion()
-    },
-    addNewQuestion () {
+    addQuestion () {
       this.quiz.questions.push({ name: '' })
     },
-    applyChangedQuestion (question, index) {
+    questionChange (question, index) {
       this.quiz.questions[index] = question
     }
   }
 }
 </script>
 <style>
-.form-wrapper {
-  margin-top: 20px;
-  min-height: 20px;
-  padding: 19px;
-  margin-bottom: 20px;
-  background-color: #f5f5f5;
-  border: 1px solid #e3e3e3;
-  border-radius: 4px;
-  box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
-}
 
-.form-row{
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-}
-
-.center{
-  margin: 0 auto;
-}
-
-.flex-center{
-  display: flex;
-  justify-content: center;
-}
 </style>
