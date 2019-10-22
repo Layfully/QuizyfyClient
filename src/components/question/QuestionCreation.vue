2c734e9862d6742291d7a629fff8018701db31b0
Adrian Gaborek <adrian.gaborek@student.up.krakow.pl>
2019-10-22 11:59:08 +0200

    Merge pull request #11 from refactor
    * refactor: move quiz create route over default quiz route to enable access to it.
    
    * feat: upgrade input field by adding ability to outline and make it denser.
    
    * feat: add input image component.
    
    * refactor: get rid of old creator components.
    
    * refactor: introduce new components for quiz creator, which use vuetify components and look better.

---
+++ src/components/question/QuestionCreation.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card class="my-5">
+    <v-card-title>
+      Pytanie {{index + 1}}
+    </v-card-title>
+    <v-card-text>
+        <InputField
+          name="Treść pytania"
+          type="text"
+          outlined
+          dense
+          :validationRules="{ require:true }"
+          @change="emitQuestionChangeEvent()"
+          v-model="question.text"/>
+        <ChoiceCreation v-for="(choice, index) in question.choices" :index=index :key=index @choiceChange="choiceChange()"/>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" @click="addChoice()">Dodaj odpowiedź</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import InputField from '@/components/InputField'
+import ChoiceCreation from '@/components/choice/ChoiceCreation'
+
+export default {
+  name: 'QuestionCreation',
+  components: {
+    InputField,
+    ChoiceCreation
+  },
+  props: { index: { required: true } },
+  data () {
+    return {
+      question: {
+        text: '',
+        choices: []
+      }
+    }
+  },
+  methods: {
+    addChoice () {
+      this.question.choices.push({ text: '', isRight: false })
+    },
+    emitQuestionChangeEvent () {
+      this.$emit('questionChange', this.question, this.index)
+    },
+    choiceChange (choice, index) {
+      this.question.choices[index] = choice
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
