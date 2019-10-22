0e0f061c1159d65e4e5a0df455befde81da7ce30
Adrian Gaborek <unity3d09@gmail.com>
2019-10-22 16:45:52 +0200

    feat: add 3 new layouts.

---
+++ src/components/layouts/Large.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs11 sm9 md6>
+                <slot/>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Large'
+}
+</script>
---
+++ src/components/layouts/None.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container fluid fill-height>
+    <slot/>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'None'
+}
+</script>
