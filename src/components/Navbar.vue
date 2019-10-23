26c185d88f7fa3a90e79f564da24afce7611d10e
Adrian Gaborek <unity3d09@gmail.com>
2019-10-23 21:13:29 +0200

    refactor: remove unnecessary props, map getters for menuItems and change classses to those provided by vuetify.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,14 +5,14 @@
         </v-app-bar-nav-icon>
       </span>
       <v-toolbar-title>
-        <router-link to="/" tag="span" class="logo">
+        <router-link to="/" tag="span" class="navbar--logo--interactive">
           <v-img contain class="hidden-sm-and-down" max-height="60" max-width="60" src="@/assets/logo.jpg"></v-img>
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <div class="hidden-sm-and-down">
         <v-btn color="error" v-for="item in activeMenuItems" :key="item.title" :to='item.path' class="mx-2">
-          <component :is='item.icon' class="icon"></component>
+          <component :is='item.icon' class="title mt-n1 mr-1"></component>
           {{ item.title }}
         </v-btn>
       </div>
@@ -34,12 +34,6 @@ export default {
     Logout,
     BookMultiple
   },
-  props: {
-    menuItems: {
-      type: Array,
-      required: true
-    }
-  },
   methods: {
     ...mapActions('User', ['logout']),
     toggleDrawer () {
@@ -47,7 +41,10 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('User', ['loggedin', 'user']),
+    ...mapGetters({
+      loggedin: 'User/loggedin',
+      menuItems: 'menuItems'
+    }),
     activeMenuItems () {
       return this.menuItems.filter(i => i.requireAuth === this.loggedin)
     }
