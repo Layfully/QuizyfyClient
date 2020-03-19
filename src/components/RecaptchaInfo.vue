5b0d62be86301258a560c3716077836652b25e11
Adrian Gaborek <unity3d09@gmail.com>
2020-03-19 18:35:05 +0100

    create recaptcha info component.

---
+++ src/components/RecaptchaInfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-alert dense type="info" class="ma-0 mt-2 py-3">
+    <p class="caption ma-0">This site is protected by invisible reCAPTCHA.
+        <br/>
+        Google
+        <v-btn outlined tile x-small href="https://policies.google.com/privacy">Privacy Policy</v-btn> and
+        <v-btn outlined tile x-small href="https://policies.google.com/terms">Terms of Service</v-btn> apply.
+    </p>
+  </v-alert>
+</template>
